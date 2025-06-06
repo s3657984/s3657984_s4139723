e7452d06b4a3a949267d6edec0e678a0eb8d2490
Yvonne <jyvonne99@icloud.com>
2025-06-06 20:06:12 +1000

    starter code

---
+++ programming_class_4/demo.py
@@ -0,0 +1,15 @@
+import pyhtml
+import student_b_level_1
+import student_b_level_2
+import student_b_level_3
+
+
+pyhtml.need_debugging_help=True
+
+#All pages that you want on the site need to be added as below
+pyhtml.MyRequestHandler.pages["/"]=student_b_level_1   #Page to show when someone accesses "http://localhost/"
+pyhtml.MyRequestHandler.pages["/b_level_2"]=student_b_level_2   #Page to show when someone accesses "http://localhost/studenty"
+pyhtml.MyRequestHandler.pages["/b_level_3"]=student_b_level_3   #Page to show when someone accesses "http://localhost/"
+
+#Host the site!
+pyhtml.host_site()
---
+++ programming_class_4/student_b_level_1.py
@@ -0,0 +1,33 @@
+def get_page_html(form_data):
+    print("About to return page home page...")
+    page_html=f"""<!DOCTYPE html>
+    <html>
+
+    <head>
+    <!-- Include the external css file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a href="/">Level1</a>
+  <a href="/b_level_2">level2</a>
+  <a href="/b_level_3">level3</a>
+</div>
+
+<div class="content">
+  <h2>CSS Template</h2>
+  <p>A topnav, content and a footer.</p>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
+"""
+    return page_html
